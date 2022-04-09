8dc1fb7714ae880f829d68815eb4d0fca6e8388f
Kaveen Abeyrathne <43596628+SuiCxDe007@users.noreply.github.com>
2022-04-09 19:23:12 +0530

    update menu-item to navigate with reactrouter v6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import HomePage from "./pages/homepage/homepage.component";
 import './App.css';
+import {Routes, Route, useParams, Link, useNavigate} from "react-router-dom";
+
+const HatsPAge = (props) => {
+    const navigate = useNavigate();
+    console.log(props)
+    const { id } = useParams();
+    return (
+    <div>
+        <Link to='/'>go home</Link>
+        <button onClick={()=> navigate('/')}>ddddd</button>
+        <h1>Hats Page {id}</h1>
+    </div>
+
+    )};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+
+        <div className="App">
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/hats" element={<HatsPAge />} />
+            </Routes>
+
+        </div>
+    );
 }
 
 export default App;
